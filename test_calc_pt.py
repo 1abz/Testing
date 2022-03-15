04ac17182ab2d23a479ac4f0e5bc9d6be4ae5c42
1abz <100859634+1abz@users.noreply.github.com>
2022-03-15 10:16:24 +0000

    new file

---
+++ test_calc_pt.py
@@ -0,0 +1,14 @@
+def test_add():
+    assert add(3, 4) == 7
+
+
+def test_divide():
+    assert divide(12, 4) == 3
+
+
+def add(number1, number2):
+    return number1 + number2
+
+
+def divide(number1, number2):
+    return number1 / number2
--- test_calc_ut.py
+++ test_calc_ut.py
@@ -10,21 +10,22 @@ class Calculator():
         return number1 + number2
 
     def divide(self, number1, number2):
-        if number2 == 0
+        if number2 == 0:
             return None
         return number1 / number2
 
-class CalculatorTesting(unittest.Testcase):
+
+class CalculatorTesting(unittest.TestCase):
 
     calc = Calculator()
 
     def test_add(self):
-        real_value = self.calc.add(4,6)
+        real_value = self.calc.add(4, 6)
         expected_value = 10
         self.assertEqual(real_value, expected_value)
 
     def test_divide(self):
-        real_value = self.calc.divide(12,2)
+        real_value = self.calc.divide(12, 2)
         expected_value = 6
         self.assertEqual(real_value, expected_value)
 
